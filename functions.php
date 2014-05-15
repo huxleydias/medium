7a69d6a801bccafd56ab8e0e8417f79a8e36bc9d
Huxley Dias <huxley.dias@hotmail.com>
2014-05-15 08:33:41 -0300

    adicionando campos personalizados para box author

--- functions.php
+++ functions.php
@@ -32,3 +32,70 @@ function new_excerpt_more($more) {
 add_filter('excerpt_more', 'new_excerpt_more');
 }	
 ?>
+
+<?php
+// CAMPOS DE PERFIL PERSONALIZADOS
+add_action( 'show_user_profile', 'my_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'my_show_extra_profile_fields' );
+ 
+function my_show_extra_profile_fields( $user ) { ?>
+ 
+    <h3>Autor Box Informations</h3>
+ 
+    <table class="form-table">
+ 
+        <tr>
+            <th><label for="twitter">Twitter</label></th>
+ 
+            <td>
+                <input type="text" name="twitter" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'twitter', $user->ID ) ); ?>" class="regular-text" /><br />
+                <span class="description">O seu nome de usuário do Twitter (URL)</span>
+            </td>
+        </tr>
+ 
+        <tr>
+            <th><label for="facebook">Facebook</label></th>
+ 
+            <td>
+                <input type="text" name="facebook" id="facebook" value="<?php echo esc_attr( get_the_author_meta( 'facebook', $user->ID ) ); ?>" class="regular-text" /><br />
+                <span class="description">O seu perfil no Facebook (URL)</span>
+            </td>
+        </tr>
+
+        <tr>
+            <th><label for="gplus">Google+</label></th>
+ 
+            <td>
+                <input type="text" name="gplus" id="gplus" value="<?php echo esc_attr( get_the_author_meta( 'gplus', $user->ID ) ); ?>" class="regular-text" /><br />
+                <span class="description">O seu perfil no Google+ (URL)</span>
+            </td>
+        </tr> 
+
+        <tr>
+            <th><label for="linkedin">Linkedin</label></th>
+ 
+            <td>
+                <input type="text" name="linkedin" id="linkedin" value="<?php echo esc_attr( get_the_author_meta( 'linkedin', $user->ID ) ); ?>" class="regular-text" /><br />
+                <span class="description">O seu perfil no Linkedin (URL)</span>
+            </td>
+        </tr>     
+ 
+    </table>
+ 
+<?php } ?>
+<?php
+// GUARDAR E MANTER INFO DOS CAMPOS
+add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+ 
+function my_save_extra_profile_fields( $user_id ) {
+ 
+    if ( !current_user_can( 'edit_user', $user_id ) )
+        return false;
+ 
+    update_usermeta( $user_id, 'twitter', $_POST['twitter'] );
+    update_usermeta( $user_id, 'facebook', $_POST['facebook'] );
+    update_usermeta( $user_id, 'gplus', $_POST['gplus'] );
+    update_usermeta( $user_id, 'linkedin', $_POST['linkedin'] );
+}
+?>
