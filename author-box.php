e7671f87efd8203d6cbfe3a874fce093dc1e88d7
Huxley Dias <huxley.dias@hotmail.com>
2014-06-22 13:59:52 -0300

    modifincano nome d osite na escrida d obox do author

--- author-box.php
+++ author-box.php
@@ -6,11 +6,11 @@
 ?>
 <div class="widget author-box" itemscope itemtype="http://schema.org/Person">
    
-    <a href="http://plus.google.com/u/1/117772548638325354735" target="_blank" title="Huxley Dias" class="author-avatar-link">
-        <img src="<?php echo $authorImage; ?>" alt="Avatar de <?php the_author_meta('display_name', 25); ?>" itemprop="photo" width="120" height="120" class="author-avatar-img">
+    <a href="http://plus.google.com/u/1/117772548638325354735" target="_blank" title="<?php bloginfo('name');?>" class="author-avatar-link">
+        <img src="<?php echo $authorImage; ?>" alt="Avatar de <?php bloginfo('name');?>" itemprop="photo" width="120" height="120" class="author-avatar-img">
     </a>
     <h1 intemprop="name" class="author-name">
-            <a href="<?php bloginfo('url') ?>"  title="Huxley Dias"  itemprop="name"><?php bloginfo('name');?></a>
+            <a href="<?php bloginfo('url') ?>"  title="<?php bloginfo('name');?>"  itemprop="name"><?php bloginfo('name');?></a>
     </h1>
     <p class="author-description" itemprop="title"><?php bloginfo('description');?></p>
     <p class="author-links">
